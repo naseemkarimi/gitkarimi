8084f9d68f2853a5d87e39949af908d6e483f5b0
Naseem <naseem89k@gmailcom>
2021-03-13 21:28:45 -0500

    last time agian

---
+++ src/test/java/com/bit/test/RESTAPITest.java
@@ -0,0 +1,18 @@
+package com.bit.test;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	@Test
+	public void postjira() {
+		System.out.println("postjira");
+		
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+		
+	}
+	
+
+}
